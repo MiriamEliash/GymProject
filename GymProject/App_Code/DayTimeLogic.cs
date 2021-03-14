d500b888083db61985c5deb61ca537016ce218e1
מרים אליאש <masha.eliash@amit365.org.il>
2021-03-14 18:41:31 +0200

    תחילת כתיבת שאילתות מסובכות של הצגה מנהל

--- GymProject/App_Code/DayTimeLogic.cs
+++ GymProject/App_Code/DayTimeLogic.cs
@@ -62,7 +62,13 @@ namespace GymProject.App_Code
             return dal.excuteQuery(sql);
         }
 
-
+       /* public int codeDayTime(int day, DateTime hour, int codeClass, string InstructorId)//שאילתה שמוצאת למנהל את הקוד של אותו חוג עם המדריך, היום והשעה
+        {
+            string sql = string.Format("SELECT DayTime.code, DayTime.codeClassTeacher ,DayTime.day1, DayTime.hour1,TeacherClass.Code, TeacherClass.CodeClass,TeacherClass.id FROM(DayTime INNER JOIN TeacherClass ON DayTime.codeClassTeacher=TeacherClass.Code WHERE DayTime.day1={0} AND DayTime.hour1={1} AND DayTime.TeacherClass.CodeClass={2} AND TeacherClass.id={3}", day, hour, codeClass, InstructorId);
+            DataSet ds = dal.excuteQuery(sql);
+            int s = Int32.Parse(ds.Tables[0].Rows[0].ItemArray.GetValue(0).ToString());
+            return s;
+        }*/
 
         /* public DataSet getNameByCodeClassTeacher(int CodeClassTeacher)
          {
